1699c560e1203b8ae98fcb5d1aa2ffd27005ffcb
Jack Dempsey <jackdempsey@hey.com>
2023-12-25 14:21:53 -0500

    create base project sanity schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
-
+import schemas from "./sanity/schemas";
 const config = defineConfig({
     projectId: 'ry1avrwn',
     dataset: 'production',
     title: 'Salon Monorepo',
     apiVersion: '2023-12-18',
     basePath: '/admin',
-    plugins: [deskTool()]
+    plugins: [deskTool()],
+    schema: {types: schemas}
 })
 
 
